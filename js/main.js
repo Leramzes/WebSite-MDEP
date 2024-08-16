10f5182441c545173f010d430ffb4a4a2755a05d
Leramzes <U21205359@utp.edu.pe>
2024-08-16 12:28:58 -0500

    V1.0 hojas de distrito

--- js/main.js
+++ js/main.js
@@ -24,3 +24,59 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// JavaScript para cargar contenido dinámico con AJAX
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.nav-link');
+
+    // Activar el enlace 'Historia' por defecto al cargar la página
+    const defaultContent = 'historia';
+    const defaultLink = Array.from(links).find(link => link.getAttribute('data-content') === defaultContent);
+    if (defaultLink) {
+        defaultLink.classList.add('active');
+        loadContent(defaultContent);
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            // Remover la clase 'active' de todos los enlaces
+            links.forEach(l => l.classList.remove('active'));
+
+            // Agregar la clase 'active' al enlace clickeado
+            this.classList.add('active');
+
+            // Cargar el contenido correspondiente
+            const contentToLoad = this.getAttribute('data-content');
+            loadContent(contentToLoad);
+        });
+    });
+
+    function loadContent(content) {
+        let url;
+        switch (content) {
+            case 'historia':
+                url = 'content/distrito/historia.html';
+                break;
+            case 'himno':
+                url = 'content/distrito/himno.html';
+                break;
+            case 'playas':
+                url = 'content/distrito/playas.html';
+                break;
+            case 'lugares':
+                url = 'content/distrito/lugares.html';
+                break;
+            default:
+                url = '';
+        }
+
+        if (url) {
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    document.querySelector('#main-content').innerHTML = data;
+                });
+        }
+    }
+});
